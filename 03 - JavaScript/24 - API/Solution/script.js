e71d3e6854e75e908b8326e65fb077a1d364ea69
elas28 <ela-saleta@hotmail.com>
2023-10-12 21:03:18 +0200

    11

--- 03 - JavaScript/24 - API/Solution/script.js
+++ 03 - JavaScript/24 - API/Solution/script.js
@@ -1,2 +1,12 @@
 const apiUrl = `https://catfact.ninja/fact`;
 
+document.querySelector("button").addEventListener("click", () => {
+    fetch(apiUrl, requestOptions)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        } else {
+          return response.json();
+        }
+      });
+    })
